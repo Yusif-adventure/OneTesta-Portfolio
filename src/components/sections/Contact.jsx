8d6f6aeea88b7b5fa1f62c3e7353911e39d2d512
Yusif Ibrahim <yibrahim021@st.ug.edu.gh>
2025-09-07 22:55:06 -0700

    Add id attribute to form in Contact component

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -51,7 +51,7 @@ const PUBLIC_KEY = "d0U66sMSqxSpwdy9T";
         <div className="px-4 w-150">
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                 Get In Touch </h2>
-                <form action="" className="space-y-6" onSubmit={handleSubmit}>
+                <form action="" id="form" className="space-y-6" onSubmit={handleSubmit}>
                     <div className="relative">
                         <input type="text"
                          id="name"  name="name"
@@ -86,7 +86,7 @@ const PUBLIC_KEY = "d0U66sMSqxSpwdy9T";
                          onChange={(e)=>setFormData({...formData,message: e.target.value})}
 />
                     </div>
-                    <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Send Message</button>
+                    <button type="submit"  className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Send Message</button>
                 </form>
         </div>
    </section> );
